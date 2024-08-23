59aba5b71cec173fcef3e5a49e725f22af341df2
Huy Hoang Nguyen <nguyenhhoang.hhn@gmail.com>
2024-08-23 16:08:08 -0400

    Add Java 02

---
+++ Java_02/src/ViDu.java
@@ -0,0 +1,12 @@
+public class ViDu {
+	public static void main(String[] args) {
+		String thongBao = "Hello World";
+		String hoVaTen = "Huy Hoang Nguyen";
+		String tenSach;
+		tenSach = "Lap trinh Java";
+		
+		System.out.println("Thong bao: " + thongBao);
+		System.out.println("Ho va ten: " + hoVaTen);
+		System.out.println("Ten sach: " + tenSach);
+	}
+}
