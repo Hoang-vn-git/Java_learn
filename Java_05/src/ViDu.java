dc58866fdb8ffb88a1e52cb88ab2725ed0641068
Huy Hoang Nguyen <nguyenhhoang.hhn@gmail.com>
2024-08-23 21:21:03 -0400

    Java 05

---
+++ Java_05/src/ViDu.java
@@ -0,0 +1,17 @@
+/**
+ * @author henry
+ */
+public class ViDu {
+	/**
+	 * @param args: tham số đầu vào
+	 */
+	public static void main(String[] args) {
+		// khai báo biến
+		int x = 10;
+		/*
+		 *Xuất số x ra màn hình 
+		 *sử dụng hàm sysout
+		 */
+		System.out.println("x = " + x);
+	}
+}
