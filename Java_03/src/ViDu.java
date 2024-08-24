e711eb29be7f49d7730df1bc1a05329d1dd1acae
Huy Hoang Nguyen <nguyenhhoang.hhn@gmail.com>
2024-08-23 21:01:31 -0400

    Add Java 03

---
+++ Java_03/src/ViDu.java
@@ -0,0 +1,14 @@
+public class ViDu {
+	public static void main(String[] args) {
+		String tenSach = "Lap trinh Java";
+		int namXuatBan = 2021;
+		double giaTien = 15.99;
+		boolean conSach = true;
+		char maKho = 'M';
+		System.out.println("Ten sach: " + tenSach);
+		System.out.println("Nam: " + namXuatBan);
+		System.out.println("Gia tien: " + giaTien);
+		System.out.println("Con sach: " + conSach);
+		System.out.println("Ma kho: " + maKho);
+	}
+}
